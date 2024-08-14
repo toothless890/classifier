9a6e468b8d652bc504e94570a3410a9722a6cc27
toothless890 <ashton.minden@gmail.com>
2024-08-14 13:08:25 -0700

    please learn and get better

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ disc_Y = modelBuilder.get_discriminator(name="discriminator_Y")
 model = modelBuilder.CycleGan(
     generator_G=gen_G, generator_F=gen_F, discriminator_X=disc_X, discriminator_Y=disc_Y
     )
-scheduler = keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=0.0003,decay_steps=2000,decay_rate=0.9)
+scheduler = keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=0.0003,decay_steps=3000,decay_rate=0.9)
 
 try:
     model.load_weights(checkpoint_filepath)
@@ -161,10 +161,10 @@ except Exception as e:
     
     
 model.compile(
-    gen_G_optimizer=keras.optimizers.AdamW(learning_rate=scheduler, beta_1=0.6),
-    gen_F_optimizer=keras.optimizers.AdamW(learning_rate=scheduler, beta_1=0.6),
-    disc_X_optimizer=keras.optimizers.AdamW(learning_rate=scheduler, beta_1=0.6),
-    disc_Y_optimizer=keras.optimizers.AdamW(learning_rate=scheduler, beta_1=0.6),
+    gen_G_optimizer=keras.optimizers.AdamW(learning_rate=scheduler, beta_1=0.5),
+    gen_F_optimizer=keras.optimizers.AdamW(learning_rate=scheduler, beta_1=0.5),
+    disc_X_optimizer=keras.optimizers.AdamW(learning_rate=scheduler, beta_1=0.5),
+    disc_Y_optimizer=keras.optimizers.AdamW(learning_rate=scheduler, beta_1=0.5),
     gen_loss_fn=modelBuilder.generator_loss_fn,
     disc_loss_fn=modelBuilder.discriminator_loss_fn,
 )
