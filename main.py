1efc36f1eeafd6a79ab6f26574c1f8bf31e8d5a0
toothless890 <ashton.minden@gmail.com>
2024-08-22 23:06:12 -0700

    i dont think it can handle it

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ NUM_CLASSES = len(CLASSNAMES)
 
 IMAGE_SIZE=variables.IMAGE_SIZE
 #usually scaled in powers of 2, reduce this number if running out of vram. increase for faster epochs
-BATCH_SIZE = 2
+BATCH_SIZE = 1
 
 INPUTSHAPE = variables.INPUTSHAPE  
 RESHAPE = variables.RESHAPE
